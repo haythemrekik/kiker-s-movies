ba4dd1dfa557e716b7e3cc86b98cb22df5bc3db8
haythem rekik <drrekik@gmail.com>
2026-04-27 23:47:58 +0100

    UI: Revert footer logo size to 240px

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -13,8 +13,8 @@ export function Footer() {
             <Image 
               src="/logo12.png" 
               alt="OneView" 
-              width={320} 
-              height={107} 
+              width={240} 
+              height={80} 
               className={styles.logo} 
             />
             <p className={styles.tagline}>La marketplace vidéo sécurisée</p>
